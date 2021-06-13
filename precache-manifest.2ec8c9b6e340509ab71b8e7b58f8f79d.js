77d5d9593ed995c985f3e61ec6b69cab3e4ffe83
RAHUL KUMAR <rahulkr705018@gmail.com>
2021-06-13 13:45:33 +0530

    Updates

---
+++ precache-manifest.2ec8c9b6e340509ab71b8e7b58f8f79d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8133c21329c181a3345b406dc87a1af",
+    "url": "/vilakshan-coder/model-react-app/index.html"
+  },
+  {
+    "revision": "be6397058e197e703e33",
+    "url": "/vilakshan-coder/model-react-app/static/css/main.57ba217b.chunk.css"
+  },
+  {
+    "revision": "e27fded2a80cd8e9b99d",
+    "url": "/vilakshan-coder/model-react-app/static/js/2.d536ee95.chunk.js"
+  },
+  {
+    "revision": "be6397058e197e703e33",
+    "url": "/vilakshan-coder/model-react-app/static/js/main.53fcb2f6.chunk.js"
+  },
+  {
+    "revision": "a9171fcaef877457e7b1",
+    "url": "/vilakshan-coder/model-react-app/static/js/runtime-main.4161c707.js"
+  }
+]);
